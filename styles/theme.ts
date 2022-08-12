38138baceabf992911a794af696cefee4efc7b82
Mark Rasavong <rasavong.mark@gmail.com>
2022-08-12 17:09:32 +0200

    style: assign global colour schemes

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,23 +1,49 @@
 export interface CoachMeTheme {
-  color: {
-    dBlue: 'dodgerblue';
-  };
-  fonts: string[];
-  fontSize: {
-    small: string;
-    medium: string;
-    large: string;
-  };
+	color: {
+		bkgHeroPurple: '#7a38ff'
+		litePurple: '#eceaff'
+		litePink: '#ffb5c6'
+		liteYellow: '#fff0cc'
+		liteOrange: '#fff0e4'
+		liteBlue: '#ff6902'
+		purple: '#fd1b4f'
+		pink: '#fd1b4f'
+		yellow: '#face65'
+		orange: '#ff6902'
+		blue: '#00a3ef'
+		gray: '#f5f5f7'
+		offlineRed: '#f02020'
+		green: '#6dd22e'
+	}
+	fonts: string[]
+	fontSize: {
+		small: string
+		medium: string
+		large: string
+	}
 }
 
 export const theme: CoachMeTheme = {
-  color: {
-    dBlue: 'dodgerblue',
-  },
-  fonts: ['sans-serif'],
-  fontSize: {
-    small: '1em',
-    medium: '2em',
-    large: '3em',
-  },
-};
+	color: {
+		bkgHeroPurple: '#7a38ff',
+		litePurple: '#eceaff',
+		litePink: '#ffb5c6',
+		liteYellow: '#fff0cc',
+		liteOrange: '#fff0e4',
+		liteBlue: '#ff6902',
+		purple: '#fd1b4f',
+		pink: '#fd1b4f',
+		yellow: '#face65',
+		orange: '#ff6902',
+		blue: '#00a3ef',
+		gray: '#f5f5f7',
+		offlineRed: '#f02020',
+		green: '#6dd22e',
+	},
+	fonts: ['sans-serif'],
+	fontSize: {
+		small: '1em',
+		medium: '2em',
+		large: '3em',
+	},
+}
