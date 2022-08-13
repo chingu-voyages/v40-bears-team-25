9ae09fa652dda8b66cec44c39dd2170c453c5138
Andrea Franceschini <andrea.franceschini03@gmail.com>
2022-08-13 17:50:18 +0200

    feature: connecting to mongoose

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,13 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import connectMongo from '../../../lib/connectMongo'
 
-export default function handleSignup(
+export default async function handleSignup(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
+	const error = await connectMongo()
 	if (req.method !== 'POST') {
 		return res
 			.status(400)
 			.send('You can only send POST requests to this endpoint')
+	}
+
+	if (error) {
+		console.error(error)
+		return res.json(error)
 	}
 
 	const { firstName, lastName, email, password } = req.body
