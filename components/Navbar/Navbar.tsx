93961e614daa743eaa1bee4736bf8dbe99202945
DeveloperAspire <franklinokolie86@gmail.com>
2022-08-13 22:07:52 +0100

    feature/added custom colors to the MUI theme

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
-import { NavbarContainer } from './Navbar.styled'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import { useTheme } from '@mui/material/styles'
 
-const Navbar = () => <NavbarContainer>NAV</NavbarContainer>
+const Navbar = () => {
+	const theme = useTheme()
+
+	return (
+		<Box>
+			<AppBar sx={{ padding: 2, backgroundColor: theme.custom.blue }}>
+				NavBar in Materia UI
+			</AppBar>
+		</Box>
+	)
+}
 
 export default Navbar
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,26 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app'
 import React from 'react'
-import GlobalStyles from '../styles/globals'
-import { ThemeProvider } from 'styled-components'
-import { theme } from '../styles/theme'
-import Layout from '../components/NxtComponents/Layout'
+// import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from '@mui/material'
 import { Provider } from 'react-redux'
-import { store } from '../redux/app/store'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-  <React.Fragment>
-    <GlobalStyles />
-    <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Provider>
-    </ThemeProvider>
-  </React.Fragment>
-  
-  )
-}
+import theme from '../styles/theme'
+import Layout from '../components/NxtComponents/Layout'
+import { store } from '../redux/app/store'
+import GlobalStyles from '../styles/globals'
+
+const MyApp: any = ({ Component, pageProps }: AppProps) => (
+	<>
+		<GlobalStyles />
+		<ThemeProvider theme={theme}>
+			<Provider store={store}>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</Provider>
+		</ThemeProvider>
+	</>
+)
 
 export default MyApp
