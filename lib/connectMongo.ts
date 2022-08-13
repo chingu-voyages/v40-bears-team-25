cc8cc48aa7088a21cc47d999b57d4baf385cdff6
Andrea Franceschini <andrea.franceschini03@gmail.com>
2022-08-13 19:51:57 +0200

    docs: added JSDOC

--- lib/connectMongo.ts
+++ lib/connectMongo.ts
@@ -2,6 +2,11 @@
 import mongoose from 'mongoose'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 
+/**
+ * Function to get the *MongoDB URI* to provide to mongoose.connect
+ * - If node is running in a `test` environment, the function spins up a mock server using MongoMemoryServer
+ * - If node is running in `production` or `development`, the function uses the Environment Variable
+ */
 const getMongoUri = async () => {
 	if (process.env.NODE_ENV === 'test') {
 		const mongoMemoryServer = await MongoMemoryServer.create()
@@ -11,6 +16,11 @@ const getMongoUri = async () => {
 	return process.env.MONGO_URI
 }
 
+/**
+ * Connects to MongoDB through Mongoose
+ * - If the connection is successful, it returns 0
+ * - If there is any error, it returns the error
+ */
 const connectMongo = async () => {
 	console.log('Connecting to Mongo...')
 	const MONGO_URI = await getMongoUri()
@@ -32,9 +42,6 @@ const connectMongo = async () => {
 	} catch (error) {
 		console.log(error, typeof error)
 
-		if (error instanceof Error) {
-			return error
-		}
 		return error
 	}
 }
