0fc3275c8757e8552a474669e4994cc2ba6970c5
Mark Rasavong <rasavong.mark@gmail.com>
2022-08-13 10:21:56 +0200

    refactor: change eslint rule to arrow functions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,31 @@
-import { Button, Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import React from 'react'
 import { useAppDispatch, useAppSelector } from '../redux/app/hooks'
-import { changeMe } from '../redux/features/test';
+import { changeMe } from '../redux/features/test'
 
 const Home: NextPage = () => {
-  const {value} = useAppSelector(state => state.test);
-  const dispatch = useAppDispatch();
+	const { value } = useAppSelector((state) => state.test)
+	const dispatch = useAppDispatch()
 
-  const callApi = async () => {
-    const fetchCreators = await fetch('/api/fetchCreators')
-    const res = await fetchCreators.json()
-    
-    if(res) console.log(res.data)
-  };
+	const callApi = async () => {
+		const fetchCreators = await fetch('/api/fetchCreators')
+		const res = await fetchCreators.json()
 
-  callApi();
+		if (res) console.log(res.data)
+	}
 
-  return (
-    <React.Fragment>
-      <h1>Hello World!!</h1>
-      <Typography variant='subtitle1'>{value}</Typography>
-      <Button variant='contained' onClick={() => dispatch(changeMe())}>Change My State!</Button>
-    </React.Fragment>
-  )
+	callApi()
+
+	return (
+		<>
+			<h1>Hello World!!</h1>
+			<Typography variant="subtitle1">{value}</Typography>
+			<Button variant="contained" onClick={() => dispatch(changeMe())}>
+				Change My State!
+			</Button>
+		</>
+	)
 }
 
-export default Home;
+export default Home
