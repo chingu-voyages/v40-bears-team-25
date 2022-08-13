d7acaac18241de52afb22df4bd4694c7beb08e09
Andrea Franceschini <andrea.franceschini03@gmail.com>
2022-08-13 17:47:54 +0200

    feature: different URI based on Node Environment

--- lib/connectMongo.ts
+++ lib/connectMongo.ts
@@ -1,9 +1,19 @@
 import mongoose from 'mongoose'
-
-const mongoUri = process.env.MONGO_URI
+import { MongoMemoryServer } from 'mongodb-memory-server'
 
 const connectMongo = async () => {
-	if (!mongoUri) {
+	console.log('Connecting to Mongo...')
+	let MONGO_URI: string | undefined
+
+	if (process.env.NODE_ENV === 'test') {
+		const mongoMemoryServer = await MongoMemoryServer.create()
+
+		MONGO_URI = mongoMemoryServer.getUri()
+	} else {
+		MONGO_URI = process.env.MONGO_URI
+	}
+
+	if (!MONGO_URI) {
 		console.error(
 			'Unable to find your MongoDB URI - Have you created a .env file with an ENV VAR named "MONGO_URI"?'
 		)
