3c9bab7cbeb4fa97b4f2a024ce42cd92d311621c
Andrea Franceschini <andrea.franceschini03@gmail.com>
2022-08-14 08:55:54 +0200

    refactor: added index.ts

---
+++ lib/index.ts
@@ -0,0 +1,6 @@
+import connectMongo from './connectMongo'
+import disconnectMongo from './disconnectMongo'
+import hashPassword from './hashPassword'
+import handleError from './handleError'
+
+export { connectMongo, disconnectMongo, hashPassword, handleError }
