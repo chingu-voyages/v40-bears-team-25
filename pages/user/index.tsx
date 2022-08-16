191eb9e2e54ca2e33d848c1b283d27192401bff3
Mark Rasavong <rasavong.mark@gmail.com>
2022-08-16 10:52:40 +0200

    style: create StatBox && StatItem reusable component

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import AvatarContainer from '../../components/AvatarContainer'
 import NameBadgeCol from '../../components/NameBadgeCol'
 import PageTitleDiv from '../../components/PageTitleDiv'
+import StatBox from '../../components/StatBox'
+import StatItem from '../../components/StatBox/StatItem'
 import { PageContainer } from './user.styled'
 
 const UserView = () => (
@@ -10,6 +12,11 @@ const UserView = () => (
 		<AvatarContainer avatarContent="V">
 			<NameBadgeCol name="Valentino" surname="Rossi" handleName="@valeR46" />
 		</AvatarContainer>
+		<StatBox>
+			<StatItem top="Training" center="looking" bottom="Status" />
+			<StatItem top="weight" center="69" bottom="kg" />
+			<StatItem top="BMI" center="21,1" bottom="normal" />
+		</StatBox>
 	</PageContainer>
 )
 
