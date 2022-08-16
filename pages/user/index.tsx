0d2406bc96935a2fd158abb6ea5c172df18b9aea
Mark Rasavong <rasavong.mark@gmail.com>
2022-08-16 09:19:51 +0200

    refactor: PageTitleDiv provide if no text make an empty div to right

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import PageTitleDiv from '../../components/PageTitleDiv'
+import { PageContainer } from './user.styled'
 
-const UserView = () => <div>UserView</div>
+const UserView = () => (
+	<PageContainer maxWidth="md">
+		<PageTitleDiv />
+	</PageContainer>
+)
+
 export default UserView
