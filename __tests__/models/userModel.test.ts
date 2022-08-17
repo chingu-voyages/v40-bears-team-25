69a5f231da72f84f37e8325490d2ca5b70bd8a14
Andrea Franceschini <andrea.franceschini03@gmail.com>
2022-08-17 15:41:18 +0200

    test: added virtuals tests (to be completed)

--- __tests__/models/userModel.test.ts
+++ __tests__/models/userModel.test.ts
@@ -28,50 +28,58 @@ describe('User Model', () => {
 		await database.connectMongo()
 	})
 
-	test('should not return an error if all fields are valid', async () => {
-		const validationError = await testUser(mockUser)
+	describe('validation tests', () => {
+		test('should not return an error if all fields are valid', async () => {
+			const validationError = await testUser(mockUser)
 
-		expect(validationError).toBeFalsy()
-	})
-
-	test('should return a ValidationError if email does not match regEx', async () => {
-		const error = await testUser({
-			...mockUser,
-			email: 'invalid email',
+			expect(validationError).toBeFalsy()
 		})
 
-		expect(error).toBeInstanceOf(Error.ValidationError)
-		if (error instanceof Error.ValidationError) {
-			// error.errors is an object that has as keys the
-			// names of the invalid items
-			expect(error.name).toBe('ValidationError')
-			expect(Object.keys(error.errors).includes('email')).toBe(true)
-		}
-	})
+		test('should return a ValidationError if email does not match regEx', async () => {
+			const error = await testUser({
+				...mockUser,
+				email: 'invalid email',
+			})
 
-	test('should return a ValidationError if FirstName is too short/long', async () => {
-		const shortError = await testUser({
-			...mockUser,
-			firstName: 'M',
-			lastName: 'R',
-		})
-
-		const longError = await testUser({
-			...mockUser,
-			firstName: 'aaaaaaaaaaaaaaaaaaaaaaaaaa',
-			lastName: 'aaaaaaaaaaaaaaaaaaaaaaaaaa',
-		})
-
-		const errors = [shortError, longError]
-
-		errors.forEach((error) => {
 			expect(error).toBeInstanceOf(Error.ValidationError)
-
 			if (error instanceof Error.ValidationError) {
-				const keys = Object.keys(error.errors)
-
-				expect(keys).toEqual(['firstName', 'lastName'])
+				// error.errors is an object that has as keys the
+				// names of the invalid items
+				expect(error.name).toBe('ValidationError')
+				expect(Object.keys(error.errors).includes('email')).toBe(true)
 			}
+		})
+
+		test('should return a ValidationError if FirstName is too short/long', async () => {
+			const shortError = await testUser({
+				...mockUser,
+				firstName: 'M',
+				lastName: 'R',
+			})
+
+			const longError = await testUser({
+				...mockUser,
+				firstName: 'aaaaaaaaaaaaaaaaaaaaaaaaaa',
+				lastName: 'aaaaaaaaaaaaaaaaaaaaaaaaaa',
+			})
+
+			const errors = [shortError, longError]
+
+			errors.forEach((error) => {
+				expect(error).toBeInstanceOf(Error.ValidationError)
+
+				if (error instanceof Error.ValidationError) {
+					const keys = Object.keys(error.errors)
+
+					expect(keys).toEqual(['firstName', 'lastName'])
+				}
+			})
+		})
+	})
+
+	describe('virtuals', () => {
+		test('personalTrainerData.customers should return the list of customer ids', () => {
+			expect(true).toBeTruthy()
 		})
 	})
 
