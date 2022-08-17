b22dd871c25f858b4bcaa042d72d4c78222321f5
Mark Rasavong <rasavong.mark@gmail.com>
2022-08-17 18:00:59 +0200

    feature: add more constants & helpers to utils

--- utils/constants.ts
+++ utils/constants.ts
@@ -4,3 +4,22 @@
 export const emailRegex = /^([a-z\d\.-_]+)+@(email\.com)$/
 
 export const passwordRegex = /[`!@#$%^&*()_+\-=[\]{};':"\\|,.<>?~]/
+export const wtUnits = ['kg', 'lb']
+export const htUnits = ['cm', 'in']
+export const usrStatus = ['looking', 'inactive', 'active']
+
+export const editFormInitValues = {
+	firstName: '',
+	lastName: '',
+	username: '',
+	email: '',
+	wt: '',
+	wtUnit: '',
+	ht: '',
+	htUnit: '',
+	password: '',
+	confirmPassword: '',
+	bio: '',
+	trainCategories: '',
+	trainingStatus: '',
+}
