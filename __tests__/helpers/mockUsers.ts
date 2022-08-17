9100f50a6a6f3739e198cf863d1fe3eb43dc7b74
Andrea Franceschini <andrea.franceschini03@gmail.com>
2022-08-17 15:22:19 +0200

    feature: test helpers initial commit

---
+++ __tests__/helpers/mockUsers.ts
@@ -0,0 +1,36 @@
+export default {
+	signup: {
+		firstName: 'Mike',
+		lastName: 'Ross',
+		email: 'mike.ross@pearsonspecter.com',
+		password: 'passwordTest',
+	},
+	customer: {
+		firstName: 'Mike',
+		lastName: 'Ross',
+		email: 'mike.ross@pearsonspecter.com',
+		password: 'passwordTest',
+		profile: {
+			bio: 'I am a fitness enthusiast!',
+			username: 'mikeross',
+		},
+		customerData: {
+			trainingGoal: 'Mass',
+			measurements: {
+				weight: 70,
+				height: 175,
+				unit: 'metric',
+			},
+		},
+	},
+	personalTrainer: {
+		firstName: 'Harvey',
+		lastName: 'Specter',
+		email: 'harvey.specter@pearsonspecter.com',
+		password: 'passwordTest',
+		profile: {
+			bio: 'I am a fitness enthusiast as well!',
+			username: 'harveyspecter',
+		},
+	},
+}
