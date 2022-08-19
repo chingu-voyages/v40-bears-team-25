b1ccf0573ff8d873e035425e0308a1ad82418917
Andrea Franceschini <andrea.franceschini03@gmail.com>
2022-08-19 02:08:58 +0200

    chore: index file

---
+++ models/index.ts
@@ -0,0 +1,6 @@
+import File, { FileAttrs } from './fileModel'
+import Review, { ReviewAttrs } from './reviewModel'
+import User, { UserAttrs } from './userModel'
+
+export { File, Review, User }
+export type { FileAttrs, ReviewAttrs, UserAttrs }
